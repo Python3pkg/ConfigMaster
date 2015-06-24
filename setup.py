f39b288793a2e12f7e2db20138847daabe649243
SunDwarf <eyesismine@gmail.com>
2015-06-24 22:18:08 +0100

    Add README.md

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="ConfigMaster",
-    version='1.1.0',
+    version='1.1.1',
     description="Programmatic configuration library for Python 3.",
+    author=" ",
     author_email="eyesismine@gmail.com",
+    url="https://bitbucket.org/SunDwarf/configmaster",
     packages=["configmaster"],
-    license="MIT"
+    license="MIT",
+    install_requires="PyYAML"
 )
