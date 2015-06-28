6ba67a1ebc0bc6982047a55984d57886ef1d66b4
SunDwarf <eyesismine@gmail.com>
2015-06-28 15:37:07 +0100

    Bump version number.
    --HG--
    branch : dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ConfigMaster",
-    version='1.2.2',
+    version='1.3.0',
     description="Programmatic configuration library for Python 3.",
     author=" ",
     author_email="eyesismine@gmail.com",
