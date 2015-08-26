700a145bb78aa861da435a4ec0e0e650920afc61
SunDwarf <eyesismine@gmail.com>
2015-08-26 12:54:38 +0100

    Actually fix it.

--- configmaster/__init__.py
+++ configmaster/__init__.py
@@ -1,6 +1,8 @@
 from . import JSONConfigFile, INIConfigFile, SPyCfg
 
+from .exc import FiletypeNotSupportedException
+
 try:
     from . import YAMLConfigFile
-except ImportError:
+except FiletypeNotSupportedException:
     YAMLConfigFile = lambda x : ImportError("You have not installed the PyYAML library. Install it via `pip install PyYAML`.")
