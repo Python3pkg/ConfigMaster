580203712ab89c9170cfea79dfd0b889d6c5e920
SunDwarf <eyesismine@gmail.com>
2015-08-29 13:46:15 +0100

    Coveralls, maybe?

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(
     name="ConfigMaster",
@@ -9,10 +31,14 @@ setup(
     url="https://github.com/SunDwarf/ConfigMaster",
     packages=["configmaster"],
     license="MIT",
+    tests_require=["pytest",
+                   "pytest-cov",
+                   "python-coveralls"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only"
-    ]
+    ],
+    cmdclass = {'test': PyTest}
 )
